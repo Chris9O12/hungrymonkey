b1df17c0a1bf3c9859e12c28f80ec79000086951
Chris9O12 <Cwuo1725@colliervilledragons.org>
2025-11-03 12:41:53 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    isHungry = !(isHungry)
+    isBackgroundYellow = !(isBackgroundYellow)
+})
+let isBackgroundYellow = false
+let isHungry = false
+scene.setBackgroundColor(2)
+let Monkey = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+isHungry = false
+game.onUpdateInterval(1000, function () {
+    if (isHungry) {
+        Monkey.sayText("Im Hungry")
+    } else {
+        Monkey.sayText("I aint Hungry")
+    }
+    if (isBackgroundYellow) {
+    	
+    }
+})
